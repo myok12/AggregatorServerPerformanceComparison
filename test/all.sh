b16e4e56f7704f39b1540f06e5d089c81b4b7117
Ohad Kravchick <okravchick@groupon.com>
2017-12-08 11:08:12 -0800

    Copied latest test script

--- test/all.sh
+++ test/all.sh
@@ -1,20 +1,22 @@
-sleep_time=60
-./test.sh jetty -v 1 -n 1000 -c 1
-sleep $sleep_time
-./test.sh vertx -v 1 -n 1000 -c 1
-sleep $sleep_time
-./test.sh jetty -v 1 -n 1000 -c 10
-sleep $sleep_time
-./test.sh vertx -v 1 -n 1000 -c 10
-sleep $sleep_time
-./test.sh jetty -v 1 -n 1000 -c 100
-sleep $sleep_time
-./test.sh vertx -v 1 -n 1000 -c 100
-sleep $sleep_time
-./test.sh jetty -v 1 -n 10000 -c 1000
-sleep $sleep_time
-./test.sh vertx -v 1 -n 10000 -c 1000
-sleep $sleep_time
-./test.sh jetty -v 1 -n 10000 -c 5000
-sleep $sleep_time
-./test.sh vertx -v 1 -n 10000 -c 5000
+sleep_time=120
+
+work ()
+{
+	date +%k:%M:%S
+	echo ./test.sh $@
+	./test.sh $@
+	sleep $sleep_time
+}
+
+work jetty -v 1 -t 300 -n 1000000000 -c 1
+work vertx -v 1 -t 300 -n 1000000000 -c 1
+work jetty -v 1 -t 300 -n 1000000000 -c 10
+work vertx -v 1 -t 300 -n 1000000000 -c 10
+work jetty -v 1 -t 300 -n 1000000000 -c 100
+work vertx -v 1 -t 300 -n 1000000000 -c 100
+work jetty -v 1 -t 300 -n 1000000000 -c 1000
+work vertx -v 1 -t 300 -n 1000000000 -c 1000
+
+# Failing due to timeout, so disabled.
+# work jetty -v 1 -t 300 -n 1000000000 -c 5000
+# work vertx -v 1 -t 300 -n 1000000000 -c 5000
