e07b7c87f83266a8f4161f8860fa393ac7d275fb
Ohad Kravchick <okravchick@groupon.com>
2017-12-13 09:35:54 -0800

    Support multiple runs

--- test/all.sh
+++ test/all.sh
@@ -1,4 +1,4 @@
-sleep_time=120
+sleep_time=0
 
 work ()
 {
@@ -7,42 +7,49 @@ work ()
 	sleep $sleep_time
 }
 
-echo Testing without padding
-work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 1
-work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 1
-work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 10
-work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 10
-work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 100
-work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 100
-work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 1000
-work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 1000
-echo Done testing without padding
+for loop i in {1..3}
+do
 
-sleep $sleep_time
+  echo Testing without padding $i
+  work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 1
+  work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 1
+  work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 10
+  work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 10
+  work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 100
+  work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 100
+  work jetty Network 0 -v 1 -t 300 -n 1000000000 -c 1000
+  work vertx Network 0 -v 1 -t 300 -n 1000000000 -c 1000
+  echo Done testing without padding $i
 
-pad=50000
-echo Testing with $pad padding
-work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 1
-work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 1
-work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 10
-work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 10
-work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 100
-work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 100
-work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 1000
-work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 1000
-echo Done testing with $pad padding
+  sleep $sleep_time
 
-#sleep $sleep_time
+  pad=50000
+  echo Testing with $pad padding $i
+  work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 1
+  work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 1
+  work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 10
+  work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 10
+  work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 100
+  work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 100
+  work jetty Network $pad -v 1 -t 300 -n 1000000000 -c 1000
+  work vertx Network $pad -v 1 -t 300 -n 1000000000 -c 1000
+  echo Done testing with $pad padding $i
 
-#work jetty Eventbus $pad -v 1 -t 300 -n 1000000000 -c 1
-#work vertx Eventbus $pad -v 1 -t 300 -n 1000000000 -c 1
-#work jetty Eventbus $pad -v 1 -t 300 -n 1000000000 -c 10
-#work vertx Eventbus $pad -v 1 -t 300 -n 1000000000 -c 10
-#work jetty Eventbus $pad -v 1 -t 300 -n 1000000000 -c 100
-#work vertx Eventbus $pad -v 1 -t 300 -n 1000000000 -c 100
-#work jetty Eventbus $pad -v 1 -t 300 -n 1000000000 -c 1000
-#work vertx Eventbus $pad -v 1 -t 300 -n 1000000000 -c 1000
+  #sleep $sleep_time
 
-# Failing due to timeout, so disabled.
-# work jetty -v 1 -t 300 -n 1000000000 -c 5000
-# work vertx -v 1 -t 300 -n 1000000000 -c 5000
+  #echo Testing with eventbus $i
+  #work jetty Eventbus 0 -v 1 -t 300 -n 1000000000 -c 1
+  #work vertx Eventbus 0 -v 1 -t 300 -n 1000000000 -c 1
+  #work jetty Eventbus 0 -v 1 -t 300 -n 1000000000 -c 10
+  #work vertx Eventbus 0 -v 1 -t 300 -n 1000000000 -c 10
+  #work jetty Eventbus 0 -v 1 -t 300 -n 1000000000 -c 100
+  #work vertx Eventbus 0 -v 1 -t 300 -n 1000000000 -c 100
+  #work jetty Eventbus 0 -v 1 -t 300 -n 1000000000 -c 1000
+  #work vertx Eventbus 0 -v 1 -t 300 -n 1000000000 -c 1000
+  #echo Done testing with eventbus $i
+
+  # Failing due to timeout, so disabled.
+  # work jetty -v 1 -t 300 -n 1000000000 -c 5000
+  # work vertx -v 1 -t 300 -n 1000000000 -c 5000
+
+done
